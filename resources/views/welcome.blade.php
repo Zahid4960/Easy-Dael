3099b98e89dca90eb7c1060e538f20c7460ae256
Zahid4960 <zahidcse4960@gmail.com>
2019-07-08 13:02:43 +0600

    Initial commit for the project Easy Deal

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,114 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet">
+
+        <!-- Styles -->
+        <style>
+            html, body {
+                background-color: #fff;
+                color: #636b6f;
+                font-family: 'Nunito', sans-serif;
+                font-weight: 200;
+                height: 100vh;
+                margin: 0;
+            }
+
+            .full-height {
+                height: 100vh;
+            }
+
+            .flex-center {
+                align-items: center;
+                display: flex;
+                justify-content: center;
+            }
+
+            .position-ref {
+                position: relative;
+            }
+
+            .top-right {
+                position: absolute;
+                right: 10px;
+                top: 18px;
+            }
+
+            .content {
+                text-align: center;
+            }
+
+            .title {
+                font-size: 84px;
+            }
+
+            .links > a {
+                color: #636b6f;
+                padding: 0 25px;
+                font-size: 13px;
+                font-weight: 600;
+                letter-spacing: .1rem;
+                text-decoration: none;
+                text-transform: uppercase;
+            }
+
+            .m-b-md {
+                margin-bottom: 30px;
+            }
+        </style>
+    </head>
+    <body>
+        <div class="flex-center position-ref full-height">
+            @if (Route::has('login'))
+                <div class="top-right links">
+                    @auth
+                        <a href="{{ url('/home') }}">Home</a>
+                    @else
+                        <a href="{{ route('login') }}">Login</a>
+
+                        @if (Route::has('register'))
+                            <a href="{{ route('register') }}">Register</a>
+                        @endif
+                    @endauth
+                </div>
+            @endif
+
+            <div class="content">
+                <div class="title m-b-md">
+                    Laravel
+                </div>
+
+                <div class="links">
+                    <a href="https://laravel.com/docs">Docs</a>
+                    <a href="https://laracasts.com">Laracasts</a>
+                    <a href="https://laravel-news.com">News</a>
+                    <a href="https://blog.laravel.com">Blog</a>
+                    <a href="https://nova.laravel.com">Nova</a>
+                    <a href="https://forge.laravel.com">Forge</a>
+                    <a href="https://github.com/laravel/laravel">GitHub</a>
+                </div>
+
+                <div class="container">
+                    <p style="width: 80%; padding: 5%; justify-content: center; background: grey; margin: 10% 0% 10% 0%;">
+                        Outstanding Challenges in Blockchain Technology in 2017
+                        It’s been a great year with real progress in Blockchain technology.
+                        Below is a subset of key questions and approaches that remain unsolved:
+                        Data On-Chain or Off-Chain
+                        The first generation of blockchain applications, mainly Bitcoin derivatives do not
+                        store real (non-cryptocurrency) data in the blockchain itself. Instead, they a store
+                        hashe (digital fingerprint). These hashes represent digital assets on-chain. In
+                        this way the blockchain can be used to track ownership of digital assets, or to
+                        act as an immutable log of activity. The advantage of this approach is that it is
+                        simple and privacy preserving, as the data itself is off-chain and not exposed to
+                    </p>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
